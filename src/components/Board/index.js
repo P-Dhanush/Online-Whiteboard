45fe0009d75e3b6d0a7f2b33191dc07c7632743c
P-Dhanush <dhanushpamulapati@gmail.com>
2024-06-21 08:05:16 +0530

    Initial Commit

---
+++ src/components/Board/index.js
@@ -0,0 +1,84 @@
+import { useContext, useEffect, useLayoutEffect, useRef } from "react";
+import rough from "roughjs";
+import boardContext from "../../store/board-context";
+import { TOOL_ACTION_TYPES, TOOL_ITEMS } from "../../constants";
+import toolboxContext from "../../store/toolbox-context";
+
+import classes from "./index.module.css";
+
+function Board() {
+  const canvasRef = useRef();
+  const textAreaRef = useRef();
+  const {
+    elements,
+    toolActionType,
+    boardMouseDownHandler,
+    boardMouseMoveHandler,
+    boardMouseUpHandler,
+  } = useContext(boardContext);
+  const { toolboxState } = useContext(toolboxContext);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  }, []);
+
+
+  useLayoutEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    context.save();
+
+    const roughCanvas = rough.canvas(canvas);
+
+    elements.forEach((element) => {
+      switch (element.type) {
+        case TOOL_ITEMS.LINE:
+        case TOOL_ITEMS.RECTANGLE:
+        case TOOL_ITEMS.CIRCLE:
+        case TOOL_ITEMS.ARROW:
+          roughCanvas.draw(element.roughEle);
+          break;
+        case TOOL_ITEMS.BRUSH:
+          context.fillStyle = element.stroke;
+          context.fill(element.path);
+          context.restore();
+          break;
+        default:
+          throw new Error("Type not recognized");
+      }
+    });
+
+    return () => {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+    };
+  }, [elements]);
+
+
+  const handleMouseDown = (event) => {
+    boardMouseDownHandler(event, toolboxState);
+  };
+
+  const handleMouseMove = (event) => {
+    boardMouseMoveHandler(event);
+  };
+
+  const handleMouseUp = () => {
+    boardMouseUpHandler();
+  };
+
+  return (
+    <>
+      <canvas
+        ref={canvasRef}
+        id="canvas"
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+      />
+    </>
+  );
+}
+
+export default Board;
